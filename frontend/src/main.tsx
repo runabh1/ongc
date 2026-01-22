57ae7f05ada7a0c52d9b9d5b2c9731a5feba7c91
runabh1 <runabh1@github.com>
2026-01-22 12:24:31 +0530

    Force rebuild: clear Vercel build cache

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+// Build cache buster - v2024-01-22
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
